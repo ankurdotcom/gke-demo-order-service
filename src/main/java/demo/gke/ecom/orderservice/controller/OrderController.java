db40eb32ac2de85a9869c478bb02c26173c2d6e5
Ankur Gupta <ankurdotcom@gmail.com>
2025-02-24 00:27:27 +0530

    Squashed commit of the following:
    commit ea8e86537fe3487cd10d0822739de86cbf6328d7
    Author: Ankur Gupta <ankurdotcom@gmail.com>
    Date:   Mon Feb 24 00:27:15 2025 +0530
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/main/java/demo/gke/ecom/orderservice/service/OrderService.java

--- src/main/java/demo/gke/ecom/orderservice/controller/OrderController.java
+++ src/main/java/demo/gke/ecom/orderservice/controller/OrderController.java
@@ -1,6 +1,8 @@
 package demo.gke.ecom.orderservice.controller;
 
 import demo.gke.ecom.orderservice.domain.Order;
+import demo.gke.ecom.orderservice.service.OrderService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,18 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("order/v1")
 public class OrderController {
 
+    private OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    };
+
     @PostMapping
     public ResponseEntity<Object> createOrder(@RequestBody Order order)
     {
-
+        orderService.createOrder(order);
         return  ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Order> orderDetails(@PathVariable String id)
     {
-             Order order = new Order();
-            order.setId(Long.parseLong(id));
+        Order order = orderService.getOrderDetails(Long.parseLong(id));
         return  ResponseEntity.status(HttpStatus.OK).body(order);
     }
 }
